04cd0ea823e57e262507f6b86be663f4580f8b98
Aline Caton <alinenepomuceno22@gmail.com>
2014-04-02 21:46:32 +1000

    Creating post controller views and models

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,29 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(permited_params)
+    if @post.save
+      redirect_to @post
+    else
+      flash.now[:alert] = @post.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find params[:id]
+  end
+
+  private
+
+  def permited_params
+    params.require(:post).permit(:title, :content, :user_id)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+
+  validates :title, :content, presence: true
 end
