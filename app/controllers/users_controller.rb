54e735365793aaceb9e7bb223f4da1b069e7eb61
Aline Caton <alinenepomuceno22@gmail.com>
2014-04-04 15:37:00 +1000

    Creating users form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(permited_params)
+    if @user.save
+      flash[:notice] = 'User created successfully'
+      redirect_to posts_path
+    else
+      flash.now[:alert] = @user.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def permited_params
+    params.require(:user).permit(:name, :email)
+  end
 end
